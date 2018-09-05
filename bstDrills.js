1ca852ba31b3dfce01487b6a0615777d21ceac54
MorganHuegel <mhue2910@gmail.com>
2018-09-05 14:55:39 -0400

    finished height drill

--- bstDrills.js
+++ bstDrills.js
@@ -12,8 +12,37 @@ function main() {
   BST.insert(2, 'two');
   BST.insert(5, 'five');
   BST.insert(7, 'seven');
-  // console.log(BST);
-  console.log(BST.find(8));
+  BST.insert(8, 'eight');
+  //console.log(BST);
+  // console.log(BST.find(8));
+  console.log('height:',height(BST));
 
 }
 main();
+
+
+
+/* ------------- Height of Binary Search Tree -------------- */
+
+//input: binary search tree  ex: BST
+//output: how many rows (5 for BST)
+
+//tools: remove, find, insert, .key .value .parent .left .right
+//look left and right for each node
+//as long as it is not null, increment counter and call recursively on that node
+
+//base case is no left or right
+//for each call, return 1 + left depth or 1 + right depth, whichever is bigger
+
+function height(searchTree){
+  //base case
+  if(!searchTree || !searchTree.key){
+    return 0;
+  }
+
+  const leftDepth = height(searchTree.left);
+  const rightDepth = height(searchTree.right);
+
+  return 1 + (leftDepth > rightDepth ? leftDepth : rightDepth);
+}
+
